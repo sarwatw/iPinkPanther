2cf40ee160db301749c13f22c1d5995a1a2154b8
Sarwat Ismail <sarwatw@sarwats-mini.guest.corp.microsoft.com>
2018-02-01 11:34:23 -0800

    Initial Commit

---
+++ Snapp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Snapp
+//
+//  Created by Sarwat  Ismail on 2/1/18.
+//  Copyright © 2018 Sarwat  Ismail. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
